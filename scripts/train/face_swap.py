734874ff2461f0b8aa5d0299b2d6613cc7b6e207
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2023-05-27 19:28:29 +0200

    update face swap

--- scripts/train/face_swap.py
+++ scripts/train/face_swap.py
@@ -6,28 +6,24 @@ from typing import List, Union
 
 from tqdm import tqdm
 
-import scripts.train.util
-import scripts.util
 from scripts.Subject import Subject
+from scripts.train import proxy_train, util
 from scripts.train.util import proxy_merge_mp4
 from scripts.workspace.WorkspaceEnum import WorkspaceStr
-from scripts.train import proxy_train, util
 
 
 def face_swap_train(
-        subject_src: Subject,
-        subject_dst: Subject,
-        model_dir: Path,
-        model_name: str,
-        gpu_indexes: Union[list, List[int]],
-        silent_start:
-        bool = False) -> None:
+    subject_src: Subject,
+    subject_dst: Subject,
+    model_dir: Path,
+    model_name: str,
+    gpu_indexes: Union[list, List[int]],
+    silent_start: bool = False
+) -> None:
     proxy_train.launch(subject_src, subject_dst, model_dir, model_name, gpu_indexes, silent_start)
 
 
 def setup(model_dir: Path, model_name: str, gpu_indexes: Union[list, List[int]], subject: Subject) -> None:
-    import shutil
-
     dim, quality = subject.specs()
     command = [
         "python", "auto_main.py", "--action", "pretrain",
@@ -37,8 +33,10 @@ def setup(model_dir: Path, model_name: str, gpu_indexes: Union[list, List[int]],
     command_str = " ".join(command)
     print(command_str)
     while True:
-        subprocess.Popen(f"gnome-terminal -- bash -ic 'conda activate deepfacelab; {command_str}; exec $SHELL'",
-                         shell=True)
+        subprocess.Popen(
+            f"gnome-terminal -- bash -ic 'conda activate deepfacelab; {command_str}; exec $SHELL'",
+            shell=True
+        )
         print("Press [Enter] when setup is done or [r] to retry and run setup again...")
         user_input = input().lower()
         if user_input == 'r':
@@ -59,8 +57,6 @@ def setup(model_dir: Path, model_name: str, gpu_indexes: Union[list, List[int]],
 
 
 def swap_model(model_dir: Path, model_name: str) -> None:
-    import shutil
-
     files = [file for file in model_dir.glob(f"{model_name}*")]
     for file in files:
         if file.is_dir():
@@ -75,11 +71,12 @@ def swap_model(model_dir: Path, model_name: str) -> None:
 
 
 def merge(
-        subject_src: Subject,
-        subject_dst: Subject,
-        model_dir: Path,
-        model_name: str,
-        gpu_indexes: Union[list, List[int], any]) -> None:
+    subject_src: Subject,
+    subject_dst: Subject,
+    model_dir: Path,
+    model_name: str,
+    gpu_indexes: Union[list, List[int], any]
+) -> None:
     util.proxy_merge(
         model_dir=model_dir,
         model_name=model_name,
@@ -100,19 +97,14 @@ def merge_mp4(subject_src: Subject, subject_dst: Subject) -> None:
 
 
 def launch(subjects: List[Subject], model_dir: Path, model_name: str) -> None:
-    model_name = scripts.train.util.choose_model(model_dir, model_name)
-    gpu_indexes = scripts.train.util.choose_gpu_index()
+    model_name = util.choose_model(model_dir, model_name)
+    gpu_indexes = util.choose_gpu_index()
     setup(model_dir, model_name, gpu_indexes, subjects[0])
     for i, subject_src in enumerate(subjects):
         for j, subject_dst in enumerate(subjects):
-            if i == j:
+            if i == j or subject_src.is_merged_from_is_done(subject_dst.id()):
                 continue
-            if subject_src.is_merged_from_is_done(subject_dst.id()):
-                continue
-            silent_start = True
-            if i == 0:
-                silent_start = False
-
+            silent_start = i != 0
             swap_model(model_dir, model_name)
 
             process = multiprocessing.Process(target=face_swap_train, args=(
