9542b58e133975da5bab75cfab51fb78fbece04e
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2023-05-27 19:09:45 +0200

    workspace.py refactoring

--- scripts/workspace/workspace.py
+++ scripts/workspace/workspace.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 from typing import List
 
@@ -8,11 +9,9 @@ from scripts.Subject import Subject
 
 
 def videos_to_subject(input_videos_dir: Path, output_subjects_dir: Path) -> None:
-    import shutil
-
-    if (not input_videos_dir.is_dir() or not input_videos_dir.exists()
-            or not output_subjects_dir.is_dir() or not output_subjects_dir.exists()):
-        raise NotADirectoryError(f"Error with input : 1: {input_videos_dir}, 2 : {output_subjects_dir}")
+    if not input_videos_dir.is_dir() or not input_videos_dir.exists() \
+            or not output_subjects_dir.is_dir() or not output_subjects_dir.exists():
+        raise NotADirectoryError(f"Error with input: 1: {input_videos_dir}, 2: {output_subjects_dir}")
 
     video_extensions = {
         ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".flv", ".f4v", ".f4p", ".f4a",
@@ -41,16 +40,14 @@ def create_subject_workspace(subjects_path: Path, dim: int, quality: int) -> Non
     subjects_path.joinpath(WorkspaceStr.pretrain.value).mkdir(exist_ok=True)
     subjects_path = [path for path in subjects_path.glob(WorkspaceStr.subject.value + "*")]
     for curr in subjects_path:
-        for path in (curr.joinpath(WorkspaceStr.frames.value),
-                     curr.joinpath(WorkspaceStr.aligned.value),
-                     curr.joinpath(WorkspaceStr.s_frames.value),
-                     curr.joinpath(WorkspaceStr.s_videos.value)):
+        for folder_name in (WorkspaceStr.frames, WorkspaceStr.aligned, WorkspaceStr.s_frames, WorkspaceStr.s_videos):
+            path = curr.joinpath(folder_name.value)
             path.mkdir(exist_ok=True)
         subjects.append(Subject(curr, dim, quality))
-        for i in range(1, len(subjects_path) + 1):
-            if i == subjects[-1].id():
+        for i, subject in enumerate(subjects):
+            if i == subject.id() - 1:
                 continue
-            sub = subjects[-1].merged_frames().joinpath(WorkspaceStr.dst_video.value + f"{i}")
+            sub = subject.merged_frames().joinpath(WorkspaceStr.dst_video.value + str(i + 1))
             sub.mkdir(exist_ok=True)
             sub2 = sub.joinpath(WorkspaceStr.mask.value)
             sub2.mkdir(exist_ok=True)
@@ -62,54 +59,42 @@ def clean_subjects_workspace(subjects: List[Subject]) -> None:
 
 
 def load_subjects(subjects_path: Path, dim: int, quality: int) -> List[Subject]:
-    from tqdm import tqdm
-
     subjects = []
-    tmp = [item for item in subjects_path.glob(WorkspaceStr.subject.value + "*")]
-    for curr in tqdm(tmp, total=len(tmp), desc="loading subjects", miniters=1.0, unit="subject"):
+    subjects_path_list = [item for item in subjects_path.glob(WorkspaceStr.subject.value + "*")]
+    for curr in tqdm(subjects_path_list, total=len(subjects_path_list), desc="loading subjects", miniters=1.0, unit="subject"):
         subjects.append(Subject(curr, dim, quality))
     return subjects
 
 
 def update_subjects(subjects: List[Subject]) -> None:
-    import shutil
-
-    remaining_ids = {subject.id() for subject in subjects}
     max_id = len(subjects)
-
     for subject in subjects:
         if subject.id() <= max_id:
             continue
         index = 1
         for i in range(1, max_id + 1):
-            if i in remaining_ids:
+            if i == subject.id():
                 continue
             index = i
-            remaining_ids.add(index)
-            remaining_ids.remove(subject.id())
             break
-        shutil.move(str(subject.root_dir()), str(subject.root_dir().parent.joinpath(
-            WorkspaceStr.subject.value + str(index))))
+        shutil.move(str(subject.root_dir()), str(subject.root_dir().parent.joinpath(WorkspaceStr.subject.value + str(index))))
 
 
 def benchmark_workspace(benchmark_dir: Path, models_name: List[str], max_iteration: int, delta_iteration: int) -> None:
-    import shutil
-
     if not benchmark_dir.parent.exists():
-        raise NotADirectoryError(f"Benchmark parent dir does not exists : {benchmark_dir.parent}")
+        raise NotADirectoryError(f"Benchmark parent dir does not exist: {benchmark_dir.parent}")
     try:
         benchmark_dir.mkdir(exist_ok=False)
     except FileExistsError:
-        if len([f for f in benchmark_dir.iterdir()]) > 0:
-            print(
-                "- Benchmark output directory is not empty ! \n" +
-                "  its content will be removed, save it then press any key."
-            )
+        if len(list(benchmark_dir.iterdir())) > 0:
+            print("- Benchmark output directory is not empty!\n"
+                  "  Its content will be removed, save it then press any key.")
             input(" > waiting for any key to be pressed....")
             print("-> Continuing...")
         shutil.rmtree(benchmark_dir)
         benchmark_dir.mkdir()
         benchmark_dir.joinpath(WorkspaceStr.benchmark_csv.value).touch()
+
     for model_name in models_name:
         model_dir = benchmark_dir.joinpath(model_name)
         model_dir.mkdir()
